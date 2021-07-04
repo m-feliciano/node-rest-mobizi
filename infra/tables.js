60b84b9bbb36930539140bd3a21915609a8178f5
Marcelo feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-07-04 20:54:48 -0300

    Corrigido tabelas houses/users

--- infra/tables.js
+++ infra/tables.js
@@ -7,7 +7,15 @@ class Tables {
     }
     createUsers(){
 
-        const query = "CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT, cpf_client VARCHAR(11) NOT NULL UNIQUE, email VARCHAR(40) NOT NULL UNIQUE, pass VARCHAR(100) NOT NULL, type enum('user','root') DEFAULT 'user', obs text, inserted_data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))"
+        const query = "CREATE TABLE IF NOT EXISTS users(\
+                    id INT NOT NULL AUTO_INCREMENT,\
+                    cpf_client VARCHAR(11) NOT NULL UNIQUE,\
+                    email VARCHAR(40) NOT NULL UNIQUE,\
+                    pass VARCHAR(100) NOT NULL,\
+                    type enum('user','root') DEFAULT 'user',\
+                    obs text,\
+                    inserted_data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\
+                    PRIMARY KEY(id))"
 
         this.connection.query(query, (error) => {
             if(error){
@@ -19,7 +27,17 @@ class Tables {
         })
     }
     createHouses(){
-        const query = "CREATE TABLE IF NOT EXISTS houses(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50), cep VARCHAR(8), street VARCHAR(30), num INT, imagem VARCHAR(200), cpf_clientID VARCHAR(11), PRIMARY KEY(id), CONSTRAINT fk_cpf_client FOREIGN KEY(cpf_clientID) REFERENCES users(cpf_client))"
+        const query = "CREATE TABLE IF NOT EXISTS houses(\
+                    id INT NOT NULL AUTO_INCREMENT,\
+                    name VARCHAR(50),\
+                    cep VARCHAR(8),\
+                    street VARCHAR(30),\
+                    num INT, imagem VARCHAR(200),\
+                    cpf_clientID VARCHAR(11),\
+                    PRIMARY KEY(id),\
+                    CONSTRAINT fk_cpf_client\
+                    FOREIGN KEY(cpf_clientID)\
+                    REFERENCES users(cpf_client))"
 
         this.connection.query(query, (error) => {
             if(error){
